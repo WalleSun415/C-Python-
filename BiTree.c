2e3a4ddb603c53c831132b33f7b51182862a92e7
WalleSun415 <672415342@qq.com>
2016-11-30 23:36:56 +0800

    二叉树前中后序遍历的递归实现

--- BiTree.c
+++ BiTree.c
@@ -9,3 +9,33 @@ typedef struct BiTNode  //二叉树结点结构
     TElemType data;
     struct BiTNode *lchild, *rchild;  //左右孩子指针
 }BiTNode, *BiTree;
+
+
+void PreOrderTraverse (BiTree T)  //二叉树前序遍历递归算法
+{
+    if (T==NULL)
+        return;
+    printf("%c", T->data);  //打印结点数据，或其他对结点的操作，一般二叉树结点均用于保存数据
+    PreOrderTraverse(T->lchild);
+    PreOrderTraverse(T->rchild);
+}
+
+
+void InOrderTraverse (BiTree T)  //二叉树中序遍历递归算法
+{
+    if (T==NULL)
+        return;
+    InOrderTraverse(T->lchild);
+    printf("%c", T->data);
+    InOrderTraverse(T->rchild);
+}
+
+
+void PostOrderTraverse (BiTree T)  //二叉树后序遍历递归算法
+{
+    if (T==NULL)
+        return;
+    PostOrderTraverse(T->lchild);
+    PostOrderTraverse(T->rchild);
+    printf("%c", T->data);
+}
