a624d558b7b3874db38e34cf04c38226185f0951
WalleSun415 <672415342@qq.com>
2016-12-11 23:37:28 +0800

    二叉查找树的删除操作

--- BST.c
+++ BST.c
@@ -38,7 +38,7 @@ Status SearchBST (BiTree T, int key, BiTree f, BiTree *p)
 Status InsertBST (BiTree *T, int key)
 {
     BiTree s, p;
-   if (!SearchBST(*T, key, NULL, &p))  //查找不成功
+    if (!SearchBST(*T, key, NULL, &p))  //查找不成功
     {
         s = (BiTree)malloc(sizeof(BiTNode));
         s->data = key;
@@ -53,4 +53,57 @@ Status InsertBST (BiTree *T, int key)
     }
     else
         return FALSE;
+}
+
+
+//二叉查找树T中存在关键字key时，删除该关键字的结点，并返回TRUE，否则返回FALSE
+Status DeleteBST (BiTree *T, int key)
+{
+    if (!*T)  //T为空树
+        return FALSE;
+    else
+    {
+        if ((*T)->data == key)
+            return Delete(T);
+        else if((*T)->data > key)
+            return DeleteBST(&((*T)->lchild), key);
+        else if ((*T)->data < key)
+            return DeleteBST(&((*T)->rchild), key);
+    }
+}
+
+
+//二叉查找树中删除具体结点p，并重建它的左右子树
+Status Delete (BiTree *p)
+{
+    BiTree q, s;
+    if ((*p)->lchild == NULL)  //右子树为空时，只需重接左子树
+    {
+        q = *p;
+        *p = (*p)->rchild;
+        free(q);
+    }
+    else if ((*p)->rchild == NULL)  //左子树为空时，只需重接右子树
+    {
+        q = *p;
+        *p = (*p)->lchild;
+        free(q);
+    }
+    else  //左右子树均不为空
+    {
+        q = *p;
+        s = (*p)->lchild;
+        while (s->rchild)  //查找待删除结点左子树的右侧尽头结点，此为p的前驱结点，同理，亦可查找后继结点
+        {
+            q = s;
+            s = s->rchild;
+        }
+        (*p)->data = s->data;  //将直接前驱结点替换待删除结点
+        if (q!=*p)
+            q->rchild = s->lchild;
+        else
+            q->lchild = s->lchild;
+        free(s);
+    }
+    return TRUE;
 }
